4b60ce4d802989d99a050255dfb1f2c2138f0f82
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2022-11-20 18:08:48 -0500

    added TODOs and logger for xUnit testing

--- unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
+++ unit-testing-using-dotnet-test/PrimeService/PrimeService.cs
@@ -4,12 +4,15 @@ namespace Prime.Services
 {
     public class PrimeService
     {
+        // TODO something
         public bool IsPrime(int candidate)
         {
+            // TODO add more tests
             if (candidate == 1)
             {
                 return false;
             }
+            // TODO implement exception
             throw new NotImplementedException("Not fully implemented.");
         }
     }
